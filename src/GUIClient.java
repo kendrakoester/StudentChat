27a5311926d9b4e670d545b8c527f96fdf91d7d8
DESKTOP-UJKUCBG\kendr <kendra.koester96@gmail.com>
2017-03-13 22:53:47 -0600

    no message

--- src/GUIClient.java
+++ src/GUIClient.java
@@ -6,7 +6,6 @@ import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 
